1f4d3280445bbde5ca545541265bb903eeda0466
andrew <andrewasmit@hotmail.com>
2023-05-14 07:05:01 -0500

    added custom queries for attaching donations to the user who donated them

--- app/models/donation.rb
+++ app/models/donation.rb
@@ -1,3 +1,9 @@
 class Donation < ApplicationRecord
     belongs_to :post
+
+    def donor
+        post = Post.find(self.post_id)
+        donor = User.find(post.user_id)
+        donor.username
+    end
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,9 +1,10 @@
 class Review < ApplicationRecord
     belongs_to :donation
 
-    def reviewed_user
+    def reviewed_user_id
         post = Post.find(self.reviewed_id)
         user = User.find(post.user_id)
-        user.username
+        user.id
     end
+    
 end
