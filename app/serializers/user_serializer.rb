4656d4e5f25cce6f63a4d03d2ecf0a41dabb257b
andrew <andrewasmit@hotmail.com>
2023-05-20 13:28:29 -0500

    trimmed down the return of each message from the all_chatrooms query

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -4,10 +4,16 @@ class UserSerializer < ActiveModel::Serializer
   has_many :reviews
 
   def all_chatrooms
-    byebug
     chatrooms = Chatroom.where(user_a_id: self.object.id).or(Chatroom.where(user_b_id: self.object.id))
     chatrooms.map do |chatroom|
-        chatroom.messages
+        chatroom.messages.map do |message|
+          {
+            id: message.id,
+            body: message.body,
+            user_id: message.user_id,
+            recipient_id: message.recipient_id
+          }
+        end
     end
 end
 
