f5353cda355c7783c1e654776a3598843827873a
andrew <andrewasmit@hotmail.com>
2023-05-29 20:50:32 -0500

    a general start on the form of submitting a picture. Not quite there, but its a good start to connect the backend

---
+++ client/src/components/PostForm.js
@@ -0,0 +1,36 @@
+import { useState, useRef } from 'react'
+
+function PostForm() {
+
+    const imagesRef = useRef([])
+
+    function handleUpload(e){
+        e.preventDefault();
+        const formData = new FormData();
+        const formImages = e.target.image.files
+        formData.append("post[title]", "This is a Test Post")
+        formData.append("post[description]", "This is a  description of the Test Post")
+        formData.append("post[zipcode]", "90210")
+
+        for(let i=0; i <formImages.length; i++){
+            formData.append("post[images][]", formImages[i]);
+        }
+        
+        for (const pair of formData.entries()){
+            console.log(`${pair[0]}, ${pair[1]}`)
+        }
+    }
+
+  return (
+    <div>
+        <form onSubmit={e=>handleUpload(e)}>
+            <input type="file" name="image" multiple ref={imagesRef} />
+            <button type = "submit" >
+                Upload Post
+            </button>
+        </form>
+    </div>
+  )
+}
+
+export default PostForm
