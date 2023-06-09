d844ac0953e6f771f2958ec5b6d2b13d9c332ad8
andrew <andrewasmit@hotmail.com>
2023-06-09 14:24:11 -0500

    controlled form for uploading a review is active on profile page. next step is to dynamically render it after a re-route to encourage users to write a review for that donation

--- client/src/components/Profile/ReviewForm.js
+++ client/src/components/Profile/ReviewForm.js
@@ -1,14 +1,26 @@
 import React, { useState } from 'react'
+import { useSelector } from 'react-redux';
 
 function ReviewForm() {
+    const { user } = useSelector(state=>state.user)
     const [body, setBody] = useState("")
     const [rating, setRating] = useState("**Choose Rating**")
 
     function handleSubmitReview(e){
         e.preventDefault();
+        const review = {
+            body: body,
+            rating: rating,
+            user_id: user.id,
+            reviewed_id: null,
+            donation_id: null
+        }
+
+        // Fetch to create new review
+
+        // clean up form after fetch
         setBody("");
         setRating("**Choose Rating**")
-        console.log(body)
     }
 
 
