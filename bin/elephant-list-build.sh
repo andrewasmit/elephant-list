fc6ab78c32cfa62a35e035b40378bb9b4e2b259e
andrew <andrewasmit@hotmail.com>
2023-06-12 12:47:08 -0500

    updated build commands

--- bin/elephant-list-build.sh
+++ bin/elephant-list-build.sh
@@ -2,6 +2,11 @@
 # exit on error
 set -o errexit
 
+rm -rf public
+npm install --prefix client && npm run build --prefix client
+cp -a client/build/. public/
+
+
 bundle install
 # bundle exec rake assets:precompile # These lines are commented out because we have an API only app
 # bundle exec rake assets:clean
