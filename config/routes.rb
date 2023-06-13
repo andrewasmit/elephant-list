758ff90e52da9ecf1956b0b49ae9778d4cce3eb1
andrew <andrewasmit@hotmail.com>
2023-06-12 20:40:46 -0500

    cleaned up unused routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  resources :chatrooms
-  resources :users
-  resources :donations
-  resources :messages
-  resources :reviews
-  resources :posts
+  resources :chatrooms, only: [:create, :destroy]
+  resources :users, only: [:create, :update, :index, :destroy]
+  resources :donations, only: [:create]
+  resources :messages, only: [:create, :update, :destroy]
+  resources :reviews, only: [:create, :update]
+  resources :posts, only: [:create, :index, :show]
 
   post '/login', to: 'session#create'
   delete 'logout', to: 'session#destroy'
