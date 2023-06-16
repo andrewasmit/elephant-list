9f8961c974e5013536d89be407b42d620bb3266f
andrew <andrewasmit@hotmail.com>
2023-06-15 19:28:00 -0500

    clean up

--- client/src/App.js
+++ client/src/App.js
@@ -16,90 +16,51 @@ import { fetchPosts } from "./redux/postSlice";
 import { fetchAllUsers } from "./redux/userSlice";
 
 function App() {
-  const { posts } = useSelector(state=>state.posts)
-  const { user, allUsers } = useSelector(state => state.user)
   const dispatch = useDispatch();
   const navigate = useNavigate();
-
 
   // Fetch user if signed in
   useEffect(() => {
     fetch("/me").then((res) => {
       if (res.ok) {
         res.json().then((data) => dispatch(login(data)));
-        navigate('/home');
+        navigate("/home");
       } else {
-        navigate('/home');
+        navigate("/home");
       }
     });
   }, []);
 
   // Initial fetch of all posts (whether you're signed in or not)
-  useEffect(()=>{
-      fetch('/posts')
-      .then(res=>res.json())
-      .then(data=>dispatch(fetchPosts(data)))
+  useEffect(() => {
+    fetch("/posts")
+      .then((res) => res.json())
+      .then((data) => dispatch(fetchPosts(data)));
   }, []);
 
   // Initial fetch of all users
-  useEffect(()=>{
-    fetch('/users')
-    .then(res=>res.json())
-    .then(userArray=>dispatch(fetchAllUsers(userArray)))
-  },[])
+  useEffect(() => {
+    fetch("/users")
+      .then((res) => res.json())
+      .then((userArray) => dispatch(fetchAllUsers(userArray)));
+  }, []);
 
-
- // Return of JSX (dependant on Login of User-State)
+  // Return of JSX (dependant on Login of User-State)
   return (
     <div className="App">
       <NavBar />
       <Routes>
-        <Route
-          path="/home"
-          element={
-            <HomePage />
-          }
-        />
-        <Route
-          path="/posts"
-          element={
-            <PostForm />
-          }
-        />
-        <Route
-          path="/login"
-          element={
-            <SignInForm />
-          }
-        />
-        <Route
-          path="/signup"
-          element={
-            <SignUpForm />
-          }
-        />
-        <Route
-          path="/donations"
-          element={
-            <DonationContainer />
-          }
-        />
-        <Route
-          path="/messages/*"
-          element={
-            <MessageHome />
-          }
-        />
-        <Route
-          path="/profile/*"
-          element={
-            <Profile />
-          }
-        />
+        <Route path="/home" element={<HomePage />} />
+        <Route path="/posts" element={<PostForm />} />
+        <Route path="/login" element={<SignInForm />} />
+        <Route path="/signup" element={<SignUpForm />} />
+        <Route path="/donations" element={<DonationContainer />} />
+        <Route path="/messages/*" element={<MessageHome />} />
+        <Route path="/profile/*" element={<Profile />} />
       </Routes>
       <Footer />
     </div>
   );
-};
+}
 
 export default App;
